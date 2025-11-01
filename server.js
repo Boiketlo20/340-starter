4501e2cb78f6b60c7e72fd798388f8a1bc6921b0
Boiketlo20 <nzimandeyolanda058@gmail.com>
2025-11-01 16:20:07 +0200

    Build the application

--- server.js
+++ server.js
@@ -6,9 +6,18 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts") //imports the package so we can use it
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs") //declaring the ejs will be the view engine for our app
+app.use(expressLayouts) //tells the app to use the express-ejs-layouts package
+app.set("layout", "./layouts/layout") //says when the express ejs layout goes looking for basic template for a view, it will be be found in a layouts folder, and the template will be named 'layout'
 
 /* ***********************
  * Routes
